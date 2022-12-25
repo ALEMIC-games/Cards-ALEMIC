429b723acb795ca70d5d4b92bdb5e88063154ce9
MichaelStrike <michael.strelecz@gmail.com>
2022-12-25 18:35:15 +0300

    rebasing triggers to signals

--- scripts/Card.gd
+++ scripts/Card.gd
@@ -1,26 +1,33 @@
 extends Button
 
-var action = 'teleport'
 
+export var object = 'owner'
+export var subject = 'owner'
+export var triggers_actions = {
+	'raffle_started' : 'teleport'
+}
 
-func _on_Card_toggled(button_pressed):
-	if button_pressed:
-		get_parent().raffle('player')
+func _ready():
+	#connecting actions to triggers
+	for key in triggers_actions.keys():
+		match key:
+			'raffle_started':
+				connect("toggled", self, triggers_actions[key], [object, subject])
 
+func teleport(obj, _subj):
+	match obj:
+		'owner':
+			get_parent().position = get_parent().position + Vector2.ONE*100
 # TRIGGERED
-func battle():
-	pass
-func battle_started():
-	pass
-func battle_finished():
-	pass
-func raffle():
-	pass
-func raffle_started():
-	pass
-func raffle_finished():
-	pass
-
-
-func teleport(object):
-	object.position = object.position + Vector2.ONE*100
+#func battle():
+#	pass
+#func battle_started():
+#	pass
+#func battle_finished():
+#	pass
+#func raffle():
+#	pass
+#func raffle_started():
+#	pass
+#func raffle_finished():
+#	pass
